49671bb30eeba7bc8e39d7c48e3baff1702ab784
gxj <gxj_snn@163.com>
2016-11-22 09:17:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/andriod/message/TurnActivity.java
@@ -0,0 +1,13 @@
+package com.example.andriod.message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TurnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turn);
+    }
+}
